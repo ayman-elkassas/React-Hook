f5e68aa9860224908b96cadec22df26cb3f9eec1
Ayman Elkassas <aymanelkassas88@gmail.com>
2021-03-25 17:26:13 +0200

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 
 function App() {
 
@@ -8,6 +8,7 @@ function App() {
   //   let [counter,setCounter]=useState(0,10)
   // }
   let [counter,setCounter]=useState(0,10)
+  let [title,setTitle]=useState('')
 
   //*to use "this" use arrow function or bind context this in constructor 
   //*can customize code more and make arrow function in clicking
@@ -19,10 +20,43 @@ function App() {
     setCounter(c=>c+1)
   }
 
+  let handleTitle=()=>{
+    setTitle("semi academy")
+  }
+
+  //* useEffect
+  //* componentDidMount,componentDidUpdate,componentWillUnmount is combined inside useEffect
+  //todo:useEffect can take second parameters array of dependencies
+  //* if depend [] can not update any time
+  //*then :
+  // * 1-no depend (calling every update)
+  // * 2-empty [] dep (never calling in update)
+  // * 3-update every time change dep like counter
+  useEffect(()=>{
+    console.log("inside useEffect 1");
+    document.title=title
+    
+    return()=>{
+      setTimeout(() => {
+        setTitle(" ")
+        console.log("cleanup");
+      }, 1000);
+    }
+
+  },[title])
+
+  //*can declare different times
+  useEffect(()=>{
+    console.log("inside useEffect 2");
+    document.title=`you have clicked ${counter} times`
+  },[counter])
+
   return (
     <div className="container text-center">
       <button className="btn-primary" onClick={countUp}>Count Up</button>
+      <button className="btn-success" onClick={handleTitle}>Change title</button>
       <p>{counter}</p>
+      <p>{title}</p>
     </div>
   );
 }
--- src/App_class.js
+++ src/App_class.js
@@ -6,16 +6,36 @@ class App extends Component {
   	super(props)
 
     this.state={
-      count:0
+      count:0,
+      title:""
     }
 
     this.countUp=this.countUp.bind(this)
+    this.titleChange=this.titleChange.bind(this)
+  }
+
+  componentDidMount(){
+    console.log("mounted");
+  }
+
+  componentDidUpdate(){
+    //*call when change in props or state
+    console.log("updated");
+    document.title=this.state.title
+    document.title=`you have clicked ${this.state.count} times`
+
   }
 
   //*to use "this" use arrow function or bind context this in constructor 
   countUp=()=>{
     this.setState({
       count:this.state.count+1
+    })
+  }
+
+  titleChange=()=>{
+    this.setState({
+      title:"React Hooks"
     })
   }
 
@@ -30,6 +50,7 @@ class App extends Component {
     return (
       <div className="container text-center">
         <button className="btn-primary" onClick={this.countUp}>Count Up</button>
+        <button className="btn-success" onClick={this.titleChange}>Title Change</button>
         <p>{this.state.count}</p>
       </div>
     );
